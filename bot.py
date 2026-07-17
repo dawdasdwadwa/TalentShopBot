f799a4ce00febd22748211b164db4b235d7e223e
TalentShopBot <281249987+dawdasdwadwa@users.noreply.github.com>
2026-07-17 21:37:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ CONFIG = {
 }
 
 OWNER_ID             = 1500198262026539099
-SHOP_IMAGE_LINK      = "https://postimg.cc/gallery/KbG4BCR"
+SHOP_IMAGE_LINK      = "https://i.postimg.cc/bvbKd68b/image.png"
 DAILY_PURCHASE_LIMIT = 10
 TICKET_CATEGORY_NAME = 'Tickets'
 TICKET_COOLDOWN_SECONDS = 5
